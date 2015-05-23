ed2b3482065d7942033c77853f4914ef7e85a47a
Tado87 <Tado87@farmcraft.fr>
2015-05-23 17:44:56 +0200

    rajoute plugin.yml

--- src/fr/farmcraft/farmcraftEco/FarmcraftEco.java
+++ src/fr/farmcraft/farmcraftEco/FarmcraftEco.java
@@ -5,12 +5,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class FarmcraftEco extends JavaPlugin {
 
-	
 	public void onEnable() {
-		System.out.println("Lancement de farmcraftEco  Version 0.0.1");
+		System.out.println("Lancement de FarmcraftEco  Version 0.0.1");
+
 	}
 	
 	public void onDisable() {
 		System.out.println("Le plugin ce coupe");
 	}
+	
 }
